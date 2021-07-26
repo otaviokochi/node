1bbaefa74ab9e9329b808bebac9aa5d3cb307af0
otavio <otaviokochi@gmail.com>
2021-07-26 18:24:33 -0300

    corrigindo

--- src/index.js
+++ src/index.js
@@ -1,173 +1,83 @@
 const express = require("express");
-const cors = require("cors");
 
-const { v4: uuidv4, validate } = require("uuid");
+const { v4: uuid } = require("uuid");
 
 const app = express();
+
 app.use(express.json());
-app.use(cors());
 
-const users = [];
+const repositories = [];
 
-function checksExistsUserAccount(request, response, next) {
-  const { username } = request.headers;
-  const user = users.find((user) => user.username === username);
-  if (user) {
-    request.user = user;
-    return next();
-  }
-  return response.status(404).json({error:  'error'});
-}
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
 
-function checksCreateTodosUserAvailability(request, response, next) {
-  const { user } = request;
-  if (user.pro || user.todos.length < 10) {
-    return next();
-  }
-  return response.status(403).json({error:  'error'});
-}
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body;
 
-function checksTodoExists(request, response, next) {
-  const { username } = request.headers;
-  const { id } = request.params;
-  const user = users.find((user) => user.username === username);
-  if (user) {
-    if (!validate(id)) {
-      return response.status(400).json({error:  'error'});
-    }
-    const todo = user.todos.find((todo) => todo.id === id);
-    if (!todo) return response.status(404).json({error:  'error'});
-    request.user = user;
-    request.todo = todo;
-    return next();
-  }
-  return response.status(404).json({error:  'error'});
-}
-
-function findUserById(request, response, next) {
-  const { id } = request.params;
-  const user = users.find((user) => user.id === id);
-  if (user) {
-    request.user = user;
-    return next();
-  }
-  return response.status(404).json({error:  'error'});
-}
-
-app.post("/users", (request, response) => {
-  const { name, username } = request.body;
-
-  const usernameAlreadyExists = users.some(
-    (user) => user.username === username
-  );
-
-  if (usernameAlreadyExists) {
-    return response.status(400).json({ error: "Username already exists" });
-  }
-
-  const user = {
-    id: uuidv4(),
-    name,
-    username,
-    pro: false,
-    todos: [],
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
   };
 
-  users.push(user);
+  repositories.push(repository);
 
-  return response.status(201).json(user);
+  return response.status(201).json(repository);
 });
 
-app.get("/users/:id", findUserById, (request, response) => {
-  const { user } = request;
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const updatedRepository = request.body;
+  delete updatedRepository.likes
 
-  return response.json(user);
-});
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
 
-app.patch("/users/:id/pro", findUserById, (request, response) => {
-  const { user } = request;
-
-  if (user.pro) {
-    return response
-      .status(400)
-      .json({ error: "Pro plan is already activated." });
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Repository not found" });
   }
 
-  user.pro = true;
+  const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
-  return response.json(user);
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
-app.get("/todos", checksExistsUserAccount, (request, response) => {
-  const { user } = request;
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
 
-  return response.json(user.todos);
-});
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
 
-app.post(
-  "/todos",
-  checksExistsUserAccount,
-  checksCreateTodosUserAvailability,
-  (request, response) => {
-    const { title, deadline } = request.body;
-    const { user } = request;
-
-    const newTodo = {
-      id: uuidv4(),
-      title,
-      deadline: new Date(deadline),
-      done: false,
-      created_at: new Date(),
-    };
-
-    user.todos.push(newTodo);
-
-    return response.status(201).json(newTodo);
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Repository not found" });
   }
-);
 
-app.put("/todos/:id", checksTodoExists, (request, response) => {
-  const { title, deadline } = request.body;
-  const { todo } = request;
+  repositories.splice(repositoryIndex, 1);
 
-  todo.title = title;
-  todo.deadline = new Date(deadline);
-
-  return response.json(todo);
+  return response.status(204).send();
 });
 
-app.patch("/todos/:id/done", checksTodoExists, (request, response) => {
-  const { todo } = request;
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
 
-  todo.done = true;
+  const repositoryIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
 
-  return response.json(todo);
-});
-
-app.delete(
-  "/todos/:id",
-  checksExistsUserAccount,
-  checksTodoExists,
-  (request, response) => {
-    const { user, todo } = request;
-
-    const todoIndex = user.todos.indexOf(todo);
-
-    if (todoIndex === -1) {
-      return response.status(404).json({ error: "Todo not found" });
-    }
-
-    user.todos.splice(todoIndex, 1);
-
-    return response.status(204).json({error:  'error'});
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Repository not found" });
   }
-);
 
-module.exports = {
-  app,
-  users,
-  checksExistsUserAccount,
-  checksCreateTodosUserAvailability,
-  checksTodoExists,
-  findUserById,
-};
+  repositories[repositoryIndex].likes++;
+
+  return response.json({likes: repositories[repositoryIndex].likes});
+});
+
+module.exports = app;
